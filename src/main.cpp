b2dff350c0812d411f221197f72cd122c9e788dc
AdityaDambhare <CouncilLeader@protonmail.com>
2026-03-03 11:38:15 +0530

    map and helper function

--- src/main.cpp
+++ src/main.cpp
@@ -42,11 +42,63 @@ void drop_ppm_image(std::string filename,std::vector<uint32_t>&buffer,const size
     ofs.close();
 }
 
+void draw_rectangle(std::vector<uint32_t>&image,const size_t image_h,const size_t image_w,
+    const size_t x,const size_t y,const size_t rec_h,const size_t rec_w, const uint32_t color)
+{
+    assert(image.size()==image_h*image_w);
+    for(int i  = 0;i<rec_w;i++){
+        for(int j = 0;j<rec_h;j++){
+            const size_t rx = x+i;
+            const size_t ry = y+j;
+            assert(rx<image_w&&ry<image_h);
+            image[ry*image_w+rx] = color;
+        }
+    }
+}
+
 int main(){
     const size_t window_height = 512;
     const size_t window_width = 512;
     std::vector<uint32_t> frame_buffer(window_width*window_height);
     fill_gradient(frame_buffer,window_width,window_height);
+
+    const size_t map_w = 16; // map width
+    const size_t map_h = 16; // map height
+    const char map[] = "0000222222220000"\
+                       "1              0"\
+                       "1     111111   0"\
+                       "1     0        0"\
+                       "0     0  1110000"\
+                       "0     3        0"\
+                       "0   10000      0"\
+                       "0   0   11100  0"\
+                       "0   0   0      0"\
+                       "0   0   1  00000"\
+                       "0       1      0"\
+                       "2       1      0"\
+                       "0       0      0"\
+                       "0 0000000      0"\
+                       "0              0"\
+                       "0002222222200000"; // our game map
+    assert(sizeof(map) == map_w*map_h+1); // +1 for the null terminated string
+
+    const size_t rect_w = window_width/map_w;
+    const size_t rect_h = window_height/map_h;
+    for (size_t j=0; j<map_h; j++) { // draw the map
+        for (size_t i=0; i<map_w; i++) {
+            if (map[i+j*map_w]==' ') continue; // skip empty spaces
+            size_t rect_x = i*rect_w;
+            size_t rect_y = j*rect_h;
+            draw_rectangle(frame_buffer, window_width, window_height, rect_x, rect_y, rect_w, rect_h,
+                pack_color(0, 255, 255));
+        }
+    }
+
+    float player_x = 4.3f;
+    float player_y = 2.345f;
+
+    draw_rectangle(frame_buffer,window_height,window_width,rect_w*player_x,rect_h*player_y,5,5,pack_color(0,0,0));
+
     drop_ppm_image("./out.ppm",frame_buffer,window_width,window_height);
     return 0;
 }

b5b847c4b04dedb31a89f5e8ecd49d4d6a889f6a
AdityaDambhare <CouncilLeader@protonmail.com>
2026-03-06 15:51:46 +0530

    texture loading

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,12 @@
 #include <cstdint>
 #include <cassert>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 #define _USE_MATH_DEFINES
 #include <math.h>
+
 
 
 //return a color value from r,g,b and alpha values
@@ -34,6 +38,44 @@ void fill_gradient(std::vector<uint32_t>&buffer,const size_t width,const size_t 
         }
     }
 }
+
+bool load_texture(const std::string filename, std::vector<uint32_t> &texture, size_t &text_size, size_t &text_cnt) {
+    int nchannels = -1, w, h;
+    unsigned char *pixmap = stbi_load(filename.c_str(), &w, &h, &nchannels, 0);
+    if (!pixmap) {
+        std::cerr << "Error: can not load the textures" << std::endl;
+        return false;
+    }
+
+    if (4!=nchannels) {
+        std::cerr << "Error: the texture must be a 32 bit image" << std::endl;
+        stbi_image_free(pixmap);
+        return false;
+    }
+
+    text_cnt = w/h;
+    text_size = w/text_cnt;
+    if (w!=h*int(text_cnt)) {
+        std::cerr << "Error: the texture file must contain N square textures packed horizontally" << std::endl;
+        stbi_image_free(pixmap);
+        return false;
+    }
+
+    texture = std::vector<uint32_t>(w*h);
+    for (int j=0; j<h; j++) {
+        for (int i=0; i<w; i++) {
+            uint8_t r = pixmap[(i+j*w)*4+0];
+            uint8_t g = pixmap[(i+j*w)*4+1];
+            uint8_t b = pixmap[(i+j*w)*4+2];
+            uint8_t a = pixmap[(i+j*w)*4+3];
+            texture[i+j*w] = pack_color(r, g, b, a);
+        }
+    }
+    stbi_image_free(pixmap);
+    return true;
+}
+
+
 //write buffer to .ppm image
 void drop_ppm_image(std::string filename,std::vector<uint32_t>&buffer,const size_t width,const size_t height){
     assert(buffer.size()==width*height);
@@ -64,6 +106,7 @@ void draw_rectangle(std::vector<uint32_t>&image,const size_t image_h,const size_
     }
 }
 
+
 int main(){
     const size_t window_height = 512;
     const size_t window_width = 1024;
@@ -72,7 +115,11 @@ int main(){
     float player_y = 2.345;
     float player_q = 1.523;//direction
     const float fov = M_PI/3.;//field of view
-
+    const size_t ncolors = 10;
+    std::vector<uint32_t> colors(ncolors);
+    for (size_t i=0; i<ncolors; i++) {
+        colors[i] = pack_color(rand()%255, rand()%255, rand()%255);
+    }
     const size_t map_w = 16; // map width
     const size_t map_h = 16; // map height
     const char map[] = "0000222222220000"\
@@ -93,22 +140,25 @@ int main(){
                        "0002222222200000"; // our game map
     assert(sizeof(map) == map_w*map_h+1); // +1 for the null terminated string
 
+    std::vector<uint32_t> walltext; // textures for the walls
+    size_t walltext_size;  // texture dimensions (it is a square)
+    size_t walltext_cnt;   // number of different textures in the image
+    if (!load_texture("./images/walltext.png", walltext, walltext_size, walltext_cnt)) {
+        std::cerr << "Failed to load wall textures" << std::endl;
+        return -1;
+    }
     const size_t rect_w = window_width/(map_w*2);
     const size_t rect_h = window_height/map_h;
 
-  
-    for(size_t frame = 0;frame<90;frame++){
-        std::stringstream ss;
-        ss << std::setfill('0') << std::setw(5) << frame << ".ppm";
-        player_q += 2*M_PI/360;
-        frame_buffer = std::vector<uint32_t>(window_height*window_width,pack_color(255,255,255));
         for (size_t j=0; j<map_h; j++) { // draw the map
             for (size_t i=0; i<map_w; i++) {
                 if (map[i+j*map_w]==' ') continue; // skip empty spaces
                 size_t rect_x = i*rect_w;
                 size_t rect_y = j*rect_h;
+                size_t icolor =  map[i+j*map_w]-'0';
+                assert(icolor<ncolors);
                 draw_rectangle(frame_buffer, window_height, window_width, rect_x, rect_y, rect_h, rect_w,
-                    pack_color(0, 255, 255));
+                    colors[icolor]);
             }
         }
         for (size_t i=0; i<window_width/2; i++) { // draw the visibility cone
@@ -123,18 +173,23 @@ int main(){
                 frame_buffer[pix_x + pix_y*window_width] = pack_color(160,160,160);
 
                 if(map[int(cx)+int(cy)*map_w]!=' '){//ray touches a wall
+                        size_t icolor = map[(int)cx+(int)cy*map_w]-'0';
+                        assert(icolor<ncolors);
                         size_t col_h = window_height/(t*cos(angle-player_q));
                         draw_rectangle(frame_buffer,window_height,window_width,window_width/2+i,window_height/2-col_h/2,
-                        col_h,1,pack_color(0,255,255)
+                        col_h,1,colors[icolor]
                         );
                         break;
                 }
-
             }
         }
-        drop_ppm_image("./images/"+ss.str(),frame_buffer,window_width,window_height);
-    }
- 
+
+    const size_t texid = 4; // draw the 4th texture on the screen
+    for (size_t i=0; i<walltext_size; i++) {
+        for (size_t j=0; j<walltext_size; j++) {
+            frame_buffer[i+j*window_width] = walltext[i + texid*walltext_size + j*walltext_size*walltext_cnt];}
+        }
+    drop_ppm_image("./out.ppm",frame_buffer,window_width,window_height); 
     return 0;
 }
 
